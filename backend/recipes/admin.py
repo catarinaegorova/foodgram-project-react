25658a30e0fadbe8a67bdee1eee84836ae567e0d
unknown <neumoin_andrey@mail.ru>
2023-05-26 13:46:54 +0300

    Filter corrected

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -23,7 +23,7 @@ class RecipeAdmin(admin.ModelAdmin):
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name', 'measurement_unit',)
-    list_filter = ('name',)
+    #  list_filter = ('name',)
     search_fields = ('name',)
     empty_value_display = '-пусто-'
 
